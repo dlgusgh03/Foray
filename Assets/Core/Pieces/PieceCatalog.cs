ef4abe03fd22f7b0d8b308b7dbcdd0e3fe6a5343
dlgusgh03 <leehyunho1104@naver.com>
2026-07-05 20:05:42 +0900

    feat: add piece pricing methods

--- Assets/Core/Pieces/PieceCatalog.cs
+++ Assets/Core/Pieces/PieceCatalog.cs
@@ -18,4 +18,39 @@ public static class PieceCatalog
                 return 0;
         }
     }
+    public static int? GetBuyPrice(PieceType type) {
+        switch (type)
+        {
+            case PieceType.King:
+                return null;
+            case PieceType.Soldier:
+                return 3;
+            case PieceType.Horse:
+                return 5;
+            case PieceType.Cannon:
+                return 7;
+            case PieceType.Chariot:
+                return 9;
+            default:
+                return null;
+        }
+    }
+    public static int? GetSellPrice(PieceType type)
+    {
+        switch (type)
+        {
+            case PieceType.King:
+                return null;
+            case PieceType.Soldier:
+                return 1;
+            case PieceType.Horse:
+                return 2;
+            case PieceType.Cannon:
+                return 3;
+            case PieceType.Chariot:
+                return 4;
+            default:
+                return null;
+        }
+    }
 }
