cdf71597d565e86d7aa93c4c7996d5fe8aaaaacb
dlgusgh03 <leehyunho1104@naver.com>
2026-07-11 14:26:00 +0900

    feat: connect run board data to battle UI

--- Assets/UI/Battle/BoardCellUI.cs
+++ Assets/UI/Battle/BoardCellUI.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class BoardCellUI : MonoBehaviour
@@ -5,11 +6,47 @@ public class BoardCellUI : MonoBehaviour
     private BoardPosition _boardPosition;
     private BoardCell _boardCell;
 
+    [SerializeField] private TMP_Text _pieceText;
+
     public BoardPosition BoardPosition => _boardPosition;
 
     public void Initialize(BoardPosition boardPosition, BoardCell boardCell)
     {
         _boardPosition = boardPosition;
         _boardCell = boardCell;
+
+        Piece piece = boardCell.Piece;
+
+        if (piece == null)
+        {
+            _pieceText.text = "";
+            return;
+        }
+
+        _pieceText.text = GetPieceText(piece.Type);
+    }
+
+    private string GetPieceText(PieceType pieceType)
+    {
+        switch (pieceType)
+        {
+            case PieceType.King:
+                return "K";
+
+            case PieceType.Soldier:
+                return "S";
+
+            case PieceType.Horse:
+                return "H";
+
+            case PieceType.Cannon:
+                return "C";
+
+            case PieceType.Chariot:
+                return "R";
+
+            default:
+                return "";
+        }
     }
 }
