1d1cf3ceeff395e0c8776658281734fc50c170bc
dlgusgh03 <leehyunho1104@naver.com>
2026-07-14 19:58:20 +0900

    feat: add augment choice UI

---
+++ Assets/UI/Scripts/Augment/AugmentChoiceUI.cs
@@ -0,0 +1,41 @@
+using System;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AugmentChoiceUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text _nameText;
+    [SerializeField] private TMP_Text _rarityText;
+    [SerializeField] private TMP_Text _descriptionText;
+    [SerializeField] private Button _selectButton;
+
+    private int _choiceIndex;
+    private System.Action<int> _onSelected;
+
+    public void Setup(Augment augment, int choiceIndex, Action<int> onSelected)
+    {
+        if (augment == null)
+        {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        gameObject.SetActive(true);
+
+        _choiceIndex = choiceIndex;
+        _onSelected = onSelected;
+
+        _nameText.text = augment.Name;
+        _rarityText.text = augment.Rarity.ToString();
+        _descriptionText.text = augment.Description;
+
+        _selectButton.onClick.RemoveAllListeners();
+        _selectButton.onClick.AddListener(OnSelectButtonClicked);
+    }
+
+    private void OnSelectButtonClicked()
+    {
+        _onSelected?.Invoke(_choiceIndex);
+    }
+}
