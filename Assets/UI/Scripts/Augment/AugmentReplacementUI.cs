56b6cee5cc0ce8556a1dc7298fdb4c9e3a7d726b
dlgusgh03 <leehyunho1104@naver.com>
2026-07-16 19:43:47 +0900

    refactor: remove duplicate augment replacement choices

--- Assets/UI/Scripts/Augment/AugmentReplacementUI.cs
+++ Assets/UI/Scripts/Augment/AugmentReplacementUI.cs
@@ -9,7 +9,6 @@ public class AugmentReplacementUI : MonoBehaviour
     [SerializeField] private TMP_Text _pendingRarityText;
     [SerializeField] private TMP_Text _pendingDescriptionText;
 
-    [SerializeField] private AugmentChoiceUI[] _ownedAugmentUIs;
     [SerializeField] private Button _skipButton;
     [SerializeField] private RunController _runController;
 
@@ -27,30 +26,8 @@ public class AugmentReplacementUI : MonoBehaviour
         _pendingRarityText.text = pendingAugment.Rarity.ToString();
         _pendingDescriptionText.text = pendingAugment.Description;
 
-        List<Augment> ownedAugments =
-            runManager.GetCurrentAugments();
-
-        for (int i = 0; i < _ownedAugmentUIs.Length; i++)
-        {
-            Augment augment =
-                i < ownedAugments.Count
-                    ? ownedAugments[i]
-                    : null;
-
-            _ownedAugmentUIs[i].Setup(
-                augment,
-                i,
-                OnOwnedAugmentSelected
-            );
-        }
-
         _skipButton.onClick.RemoveAllListeners();
         _skipButton.onClick.AddListener(OnSkipButtonClicked);
-    }
-
-    private void OnOwnedAugmentSelected(int index)
-    {
-        _runController.OnAugmentReplaced(index);
     }
 
     private void OnSkipButtonClicked()
