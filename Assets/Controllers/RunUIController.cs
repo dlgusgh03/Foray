317969ab18820bd01c1a431eb8ec8d53a307d6ea
dlgusgh03 <leehyunho1104@naver.com>
2026-07-20 15:53:10 +0900

    feat: add battle and shop layout transitions

--- Assets/Controllers/RunUIController.cs
+++ Assets/Controllers/RunUIController.cs
@@ -3,25 +3,56 @@ using UnityEngine;
 public class RunUIController : MonoBehaviour
 {
     [Header("Base Run UI")]
-    [SerializeField] private GameObject _fixedLeftUIRoot;
+    [SerializeField] private RectTransform _mainRunContentRoot;
     [SerializeField] private GameObject _goldHUDRoot;
-    [SerializeField] private GameObject _boardAreaRoot;
+    [SerializeField] private RectTransform _ownedCardUIRoot;
 
     [Header("State UI")]
     [SerializeField] private GameObject _shopUIRoot;
     [SerializeField] private GameObject _augmentSelectionUIRoot;
     [SerializeField] private GameObject _augmentReplacementUIRoot;
 
+    [Header("Battle Layout")]
+    [SerializeField] private Vector2 _battleMainContentPosition = Vector2.zero;
+    [SerializeField] private Vector2 _battleOwnedCardPosition = Vector2.zero;
+
+    [Header("Shop Layout")]
+    [SerializeField] private Vector2 _shopMainContentPosition = new Vector2(-300f, 0f);
+    [SerializeField] private Vector2 _shopOwnedCardPosition = new Vector2(-100f, 0f);
+
     public void Refresh(RunState state)
     {
         bool showBaseRunUI = state == RunState.Battle || state == RunState.Shop || state == RunState.AugmentSelection || state == RunState.AugmentReplacement;
 
-        _fixedLeftUIRoot.SetActive(showBaseRunUI);
+        _mainRunContentRoot.gameObject.SetActive(showBaseRunUI);
         _goldHUDRoot.SetActive(showBaseRunUI);
-        _boardAreaRoot.SetActive(showBaseRunUI);
+        _ownedCardUIRoot.gameObject.SetActive(showBaseRunUI);
 
         _shopUIRoot.SetActive(state == RunState.Shop);
         _augmentSelectionUIRoot.SetActive(state == RunState.AugmentSelection);
         _augmentReplacementUIRoot.SetActive(state == RunState.AugmentReplacement);
+
+        if (state == RunState.Shop)
+        {
+            ApplyShopLayout();
+        }
+        else if (showBaseRunUI)
+        {
+            ApplyBattleLayout();
+        }
+    }
+
+    private void ApplyBattleLayout()
+    {
+        _mainRunContentRoot.anchoredPosition = _battleMainContentPosition;
+
+        _ownedCardUIRoot.anchoredPosition = _battleOwnedCardPosition;
+    }
+
+    private void ApplyShopLayout()
+    {
+        _mainRunContentRoot.anchoredPosition = _shopMainContentPosition;
+
+        _ownedCardUIRoot.anchoredPosition = _shopOwnedCardPosition;
     }
 }
